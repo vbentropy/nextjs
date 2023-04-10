b25f0dd60c9a2eed6424013a4e43963b68b9cc26
entropy <myentropy@proton.me>
2023-04-10 19:04:51 -0400

    deleted fcc2

--- next.config.js
+++ next.config.js
@@ -3,4 +3,13 @@ const nextConfig = {
     reactStrictMode: true,
 }
 
-module.exports = { trailingSlash: true, nextConfig, images: { loader: "custom" } }
+module.exports = {
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+    trailingSlash: true,
+    nextConfig,
+    images: { loader: "custom" },
+}
